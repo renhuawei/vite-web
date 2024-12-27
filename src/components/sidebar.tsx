9b3ecaa2ce2915e4ebe9d64b4b8367e4cfd8f7ff
renhuawei <renhuawei@msn.com>
2024-12-28 00:38:57 +0800

    feat: make sidebar responsive and accessible

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,11 +1,10 @@
 import Nav from './nav'
-import { TooltipProvider } from './ui/tooltip'
 import { cn } from '@/lib/utils'
 import { Separator } from './ui/separator'
-import FaryLogo from '@/assets/vite.svg'
 import { Button } from './ui/button'
 import { sidelinks } from '@/data/sidelinks'
-import { IconChevronsLeft } from '@tabler/icons-react'
+import { IconChevronsLeft, IconMenu2, IconX } from '@tabler/icons-react'
+import { useState } from 'react'
 
 interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
   isCollapsed: boolean
@@ -17,39 +16,86 @@ export default function Sidebar({
   isCollapsed,
   setIsCollapsed,
 }: SidebarProps) {
+  const [navOpened, setNavOpened] = useState(false)
   return (
     <div
       className={cn(
-        'pb-12 relative hidden lg:block border-r-2 border-r-slate-100 dark:border-r-slate-900 transition-[width]',
+        `relative w-full border-r-2 border-r-slate-100 transition-[width] dark:border-r-slate-900 md:pb-12 ${isCollapsed ? 'md:w-14' : 'md:w-64'}`,
         className
       )}
     >
-      <TooltipProvider delayDuration={0}>
-        <div className='px-4 py-2 flex gap-2 h-16 items-center'>
-          <img
-            src={FaryLogo}
-            alt='Logo'
-            className={`transition-all ${isCollapsed ? 'h-6 w-6' : 'h-12 w-12'}`}
-          />
-          <div
-            className={`flex flex-col justify-end truncate ${isCollapsed ? 'w-0 invisible' : 'w-auto visible'}`}
+      <div
+        className={`flex h-16 w-full items-center justify-between gap-2 px-4 py-2`}
+      >
+        <div className='flex items-center gap-2'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 256 256'
+            className={`transition-all ${isCollapsed ? 'h-6 w-6' : 'h-8 w-8'}`}
           >
-            <span className='font-medium'>Right Direction</span>
-            <span className='text-xs'>Since 2020</span>
+            <rect width='256' height='256' fill='none'></rect>
+            <line
+              x1='208'
+              y1='128'
+              x2='128'
+              y2='208'
+              fill='none'
+              stroke='currentColor'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='16'
+            ></line>
+            <line
+              x1='192'
+              y1='40'
+              x2='40'
+              y2='192'
+              fill='none'
+              stroke='currentColor'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='16'
+            ></line>
+            <span className='sr-only'>Website Name</span>
+          </svg>
+          <div
+            className={`flex flex-col justify-end truncate ${isCollapsed ? 'invisible w-0' : 'visible w-auto'}`}
+          >
+            <span className='font-medium'>Shadcn Dashboard</span>
+            <span className='text-xs'>Vite + ShadcnUI</span>
           </div>
         </div>
-        <Separator className='mb-4' />
-        <Nav isCollapsed={isCollapsed} links={sidelinks} />
-      </TooltipProvider>
+        <Button
+          variant='ghost'
+          size='icon'
+          className='md:hidden'
+          aria-label='Toggle Navigation'
+          aria-controls='sidebar-menu'
+          aria-expanded={navOpened}
+          onClick={() => setNavOpened((prev) => !prev)}
+        >
+          {navOpened ? <IconX /> : <IconMenu2 />}
+        </Button>
+      </div>
+
+      <Separator className='mb-4 hidden md:block' />
+      <Nav
+        id='sidebar-menu'
+        className={
+          navOpened ? 'max-h-screen' : 'max-h-0 py-0 md:max-h-screen md:py-2'
+        }
+        isCollapsed={isCollapsed}
+        links={sidelinks}
+      />
       <Button
         onClick={() => setIsCollapsed((prev) => !prev)}
         size='icon'
         variant='outline'
-        className='rounded-full -right-5 top-1/2 absolute'
+        className='absolute -right-5 top-1/2 hidden rounded-full md:inline-flex'
       >
         <IconChevronsLeft
           stroke={1.5}
-          className={`w-5 h-5 ${isCollapsed ? 'rotate-180' : ''}`}
+          className={`h-5 w-5 ${isCollapsed ? 'rotate-180' : ''}`}
         />
       </Button>
     </div>
