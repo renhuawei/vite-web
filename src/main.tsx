704cbe30845797f09a7e59812e54ec98b9047060
renhuawei <renhuawei@msn.com>
2025-02-18 22:49:08 +0800

    refactor: update react-router to use new api for routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from 'react-router'
 import { RouterProvider } from 'react-router/dom'
-import Router from '@/router'
+import router from '@/router'
 import { Toaster } from '@/components/ui/toaster'
 import { ThemeProvider } from '@/components/theme-provider'
-import './index.css'
-
-const router = createBrowserRouter(
-  createRoutesFromElements(<Route path='/*' element={<Router />} />)
-)
+import '@/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
