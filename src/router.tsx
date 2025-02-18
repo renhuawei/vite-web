500a086858d4d810a223e2a2581fbb071e2e93a1
renhuawei <renhuawei@msn.com>
2025-02-18 17:17:33 +0800

    feat: add email + password sign in page

--- src/router.tsx
+++ src/router.tsx
@@ -2,11 +2,14 @@ import { Suspense, lazy } from 'react'
 import { Route, Routes } from 'react-router'
 import Loader from '@/components/loader'
 const Dashboard = lazy(() => import('@/pages/dashboard'))
+const SignIn = lazy(() => import('@/pages/auth/sign-in'))
+
 export default function Router() {
   return (
     <Suspense fallback={<Loader />}>
       <Routes>
         <Route path='/' element={<Dashboard />} />
+        <Route path='/sign-in' element={<SignIn />} />
       </Routes>
     </Suspense>
   )
