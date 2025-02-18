682a0f30d27d08155f8373b2976656d5f82bc32a
renhuawei <renhuawei@msn.com>
2025-02-18 21:44:41 +0800

    feat: add error pages

--- src/router.tsx
+++ src/router.tsx
@@ -7,15 +7,28 @@ const SignIn2 = lazy(() => import('@/pages/auth/sign-in-2'))
 const SignUp = lazy(() => import('@/pages/auth/sign-up'))
 const ForgotPassword = lazy(() => import('@/pages/auth/forgot-password'))
 
+// Errors
+const GeneralError = lazy(() => import('@/pages/errors/general-error'))
+const NotFoundError = lazy(() => import('@/pages/errors/not-found-error'))
+const MaintenanceError = lazy(() => import('@/pages/errors/maintenance-error'))
+
+// Routes
 export default function Router() {
   return (
     <Suspense fallback={<Loader />}>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
+        {/* Auth routes */}
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-in-2' element={<SignIn2 />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
+
+        <Route path='/' element={<Dashboard />} />
+
+        {/* Errors routes */}
+        <Route path='/404' element={<NotFoundError />} />
+        <Route path='/500' element={<GeneralError />} />
+        <Route path='/503' element={<MaintenanceError />} />
       </Routes>
     </Suspense>
   )
