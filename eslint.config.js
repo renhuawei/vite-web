7adedce04f550efc214466a2bc3280ddbd3291d2
renhuawei <renhuawei@msn.com>
2025-02-26 00:03:57 +0800

    chore: upgrade Vite, Prettier, and ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import eslintConfigPrettier from 'eslint-config-prettier'
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default tseslint.config(
-  { ignores: ['dist', 'src/components/ui', 'tailwind.config.js'] },
+  { ignores: ['dist', 'src/components/ui'] },
   {
     extends: [
       js.configs.recommended,
