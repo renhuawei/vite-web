0cd8e04acb8dd65afbad14b503ea8c36f6b540a1
renhuawei <renhuawei@msn.com>
2025-02-26 16:46:29 +0800

    refactor!: reorganize project to feature-based structure

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -1,8 +1,5 @@
-'use client'
-
 // Inspired by react-hot-toast library
 import * as React from 'react'
-
 import type { ToastActionElement, ToastProps } from '@/components/ui/toast'
 
 const TOAST_LIMIT = 1
@@ -15,6 +12,7 @@ type ToasterToast = ToastProps & {
   action?: ToastActionElement
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const actionTypes = {
   ADD_TOAST: 'ADD_TOAST',
   UPDATE_TOAST: 'UPDATE_TOAST',
